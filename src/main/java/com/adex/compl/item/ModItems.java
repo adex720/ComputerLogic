0227ed065c3147bc5c23fe7ac4510796c64594f9
adex720 <25402611+adex720@users.noreply.github.com>
2024-10-18 23:20:07 +0300

    Add transistor item to redstone item group

--- src/main/java/com/adex/compl/item/ModItems.java
+++ src/main/java/com/adex/compl/item/ModItems.java
@@ -4,6 +4,7 @@ import com.adex.compl.util.Util;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
@@ -12,8 +13,17 @@ public class ModItems {
 
     public static final Item TRANSISTOR = new Item(new Item.Settings());
 
+    /**
+     * Calls {@link ModItems#addItemsToItemGroups()}
+     */
     public static void registerItems() {
         Registry.register(Registries.ITEM, Util.id("transistor"), TRANSISTOR);
+
+        addItemsToItemGroups();
+    }
+
+    public static void addItemsToItemGroups() {
+        addToItemGroup(TRANSISTOR, ItemGroups.REDSTONE);
     }
 
     public static void addToItemGroup(Item item, RegistryKey<ItemGroup> itemGroup) {
